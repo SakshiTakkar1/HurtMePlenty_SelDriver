650be7bacc49e6112aeb8af7ef35edacaf9ed9d1
Sakshi_Kumari <sakshitakkar5@gmail.com>
2022-08-04 11:49:11 +0530

    commit 4 Aug,2022 1st time

--- src/main/java/Service/TestDataReader.java
+++ src/main/java/Service/TestDataReader.java
@@ -4,6 +4,8 @@ import java.util.ResourceBundle;
 
 public class TestDataReader {
 
+
+
     private static ResourceBundle resourceBundle = ResourceBundle.getBundle(System.getProperty("environment"));
 
     public static String getTestData(String key) {
